a66f82d6f051e9e133f45f0e450a1111b6506790
Dimitris Terzidis <dterzidis2008@gmail.com>
2025-12-01 19:42:58 +0200

    completed p1

--- 2025/index.py
+++ 2025/index.py
@@ -10,20 +10,55 @@ headers = {
 }
 
 response = requests.get(url, headers=headers).text
+rotations = response.splitlines()
+dial = 50
+count = 0
+
+for rotation in rotations:
+    direction = rotation[0]
+    steps = int(rotation[1:])
+    
+    # update dial
+    if direction == "L":
+        dial = (dial - steps) % 100
+    else:
+        dial = (dial + steps) % 100
+
+    if dial == 0:
+        count += 1
+
+print(count)
+
+
+# distance = distance % 100
+
+# for i in range(0, len(input)-1):
+#     distance = int(input[i].strip('RL'))
+#     initLetter = input[i][0]
+
+# if initLetter=="R":
+#     distance = 50 + distance
+#     print(distance)
+
+# if initLetter=="L":
+#     distance = 50 - distance
+
+
+
 
 
 # Store Input In List
-input = response.split('\n')
-start = 50
-for i in range(0,len(input)-1):
-    # string number 
-    sez = input[i].strip('RL')
-    sez = int(sez)
+# input = response.split('\n')
+# start = 50
+# for i in range(0,len(input)-1):
+#     # string number 
+#     sez = input[i].strip('RL')
+#     sez = int(sez)
  
-    if 'R' in input[i]:
-        if 99-start > sez :
-            pass
+#     if 'R' in input[i]:
+#         if 99-start > sez :
+#             start = -1 +sez - (99-start)
         
-    else:
-        pass
+#     else:
+#         pass
     
